cb10c705937862b5d80c20ffa5547865cf06bd91
Claudio Bonfati <claudio_bonfati@outlook.com>
2020-05-24 13:33:33 -0300

    Updates

---
+++ precache-manifest.99e382c2a4d8b1518e9cbf275dbf8e04.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e461681d9645240e1809513058bceab5",
+    "url": "/calc/index.html"
+  },
+  {
+    "revision": "eed4a57dba0efe2ad6da",
+    "url": "/calc/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "74538651a244b3cd5c5e",
+    "url": "/calc/static/css/main.5600e99d.chunk.css"
+  },
+  {
+    "revision": "eed4a57dba0efe2ad6da",
+    "url": "/calc/static/js/2.5923d74d.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/calc/static/js/2.5923d74d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74538651a244b3cd5c5e",
+    "url": "/calc/static/js/main.11e82de2.chunk.js"
+  },
+  {
+    "revision": "3f50dadc6c764efc2941",
+    "url": "/calc/static/js/runtime-main.7e8b4626.js"
+  }
+]);
