a3b54d6ba6f1e9382dc0258e1e126df2cb81a615
Claudio Bonfati <claudio_bonfati@outlook.com>
2020-05-24 13:49:38 -0300

    Added gh-pages

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -11,8 +11,7 @@
 // opt-in, read https://bit.ly/CRA-PWA
 
 const isLocalhost = Boolean(
-  window.location.hostname === '192.168.15.25' ||
-  // window.location.hostname === 'localhost' ||
+  window.location.hostname === 'localhost' ||
     // [::1] is the IPv6 localhost address.
     window.location.hostname === '[::1]' ||
     // 127.0.0.0/8 are considered localhost for IPv4.
